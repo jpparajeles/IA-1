578d26d4d4d77f87f18222bedb6b0bc7ef433924
Gandergof <ldiegopizarro@gmail.com>
2015-09-14 00:37:09 -0600

    Objeto torre, con sus respectivos movimientos siguientes mandejados como una matriz strings

---
+++ Test.py
@@ -0,0 +1,12 @@
+__author__ = 'luisdiegopizarro'
+from Towel import *
+
+d=Towel([
+['n','n','n','a'],
+['a','a','A','e'],
+['a','r','v','A'],
+['A','v','r','r'],
+['r','v','A','v']])
+
+printAllMoves(d.nextMoves)
+
---
+++ Towel.py
@@ -0,0 +1,93 @@
+__author__ = 'luisdiegopizarro'
+from copy import deepcopy
+'''
+A amarillo
+a azul
+r rojo
+v verde
+e espacio
+n barrera
+
+n n n e
+a a A a
+a r v A
+A v r r
+r v A v'''
+class Space:
+    def __init__(self,pRow,pColumn):
+        self.column=pColumn
+        self.row=pRow
+
+class Towel:
+    def __init__(self,pMatrix):
+        self.matrix=pMatrix;
+        self.emptySpace=self.setEmptySpace()
+        self.rowLen=len(pMatrix)
+        self.nextMoves=[]
+        ###################
+        self.setNextMovements()
+
+
+    def getColumn(self,numCol):
+        col=[]
+        for x in self.matrix:
+            col.append(x[numCol])
+        return col
+
+    def getRotations(self):
+        for rowNum in range(0,self.rowLen):
+            self.nextMoves.append(rotateRight(deepcopy(self.matrix),rowNum))
+            self.nextMoves.append(rotateLeft(deepcopy(self.matrix),rowNum))
+
+
+    def setEmptySpace(self):
+        #el empty aca lo tengo como 'e' si en el txt cambia tuesta, hay q ver si me lo mandan x parametro
+        for rowNum,row in enumerate(self.matrix):
+            if('e' in row):
+               return Space(rowNum,row.index('e'))#[fila,columna]
+
+    def getBallMovement(self):
+        col=self.emptySpace.column
+        row=self.emptySpace.row
+        rowsCopy=deepcopy(self.matrix)#copia necesaria para no alterar el objeto original
+        if(row==0):#mueve bola hacia abajo
+            rowsCopy[row][col],rowsCopy[row+1][col]=rowsCopy[row+1][col],rowsCopy[row][col]
+            self.nextMoves.append(rowsCopy)
+        elif(row==self.rowLen-1):#mueve bola hacia arriba
+            rowsCopy[row][col],rowsCopy[row-1][col]=rowsCopy[row-1][col],rowsCopy[row][col]
+            self.nextMoves.append(rowsCopy)
+        else:#mueve la bola hacia arriba y hacia abajo
+            rowsCopy[row][col],rowsCopy[row+1][col]=rowsCopy[row+1][col],rowsCopy[row][col]
+            rowsCopy2=deepcopy(self.matrix)#se necesita una segunda copia para no alterar la primera
+            rowsCopy2[row][col],rowsCopy2[row-1][col]=rowsCopy2[row-1][col],rowsCopy2[row][col]
+            self.nextMoves.append(rowsCopy)
+            self.nextMoves.append(rowsCopy2)
+
+    def setNextMovements(self):
+        self.getRotations()
+        self.getBallMovement()
+
+def rotateRight(towel,pos):
+    towel[pos]=towel[pos][-1:]+towel[pos][:-1]
+    return towel
+
+def rotateLeft(towel,pos):
+    towel[pos]=towel[pos][1:]+towel[pos][:1]
+    return towel
+
+
+###test prints
+def printBeauty(rows):
+        for x in rows:
+            for y in x:
+                print(y,end=" ")
+            print(" ")
+
+def printAllMoves(moves):
+    for x,y in enumerate(moves):
+        print("mov# "+str(x+1))
+        printBeauty(y)
+
+
+
+
