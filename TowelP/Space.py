8312382dc176cc4807be5aeaf8823281c95bc393
Gandergof <ldiegopizarro@gmail.com>
2015-09-15 14:38:02 -0600

    Objeto Towel list, Space es un espacio en la torre y Movement tiene un estado y su costo

---
+++ TowelP/Space.py
@@ -0,0 +1,7 @@
+__author__ = 'luisdiegopizarro'
+#este espacio es un espacio especifico en la torre
+#se utiliza para buscar el espacio en vacio
+class Space:
+    def __init__(self,pRow,pColumn):
+        self.column=pColumn
+        self.row=pRow
---
+++ TowelP/TowelTest.py
@@ -0,0 +1,11 @@
+__author__ = 'luisdiegopizarro'
+from TowelP.Towel import *
+
+d=Towel(
+[['n','n','n','e'],
+['a','a','A','a'],
+['a','r','v','A'],
+['A','v','r','r'],
+['r','v','A','v']])
+
+printAllMoves(d.nextMoves)
